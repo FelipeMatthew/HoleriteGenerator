9855f3d2e7fb814aebc28c26eede37aa955312b5
Felipe Matthew Moreira Nascimento <102431464+FelipeMatthew@users.noreply.github.com>
2022-10-26 16:05:14 -0300

    Create App.tsx

---
+++ web/src/App.tsx
@@ -0,0 +1,17 @@
+import { Footer } from './components/LandingPage/Footer';
+import { Header } from './components/LandingPage/Header';
+import { Main } from './components/LandingPage/Main';
+import './styles/main.css';
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Main/>
+      <Footer/>
+    </> 
+  )
+}
+
+
+export default App
